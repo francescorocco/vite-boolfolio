876a505e218333963d9b8bf3d0dbb4dc240cbccf
Francesco Rocco <francescorocco97@gmail.com>
2023-05-30 16:57:32 +0200

    Milestone 3, 4 completate

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
-<script setup>
+<script>
+  import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
+
+
+  export default{
+
+
+
+
+    components:{
+      AppHeader,
+      AppMain,
+    }
+  }
+
 </script>
 
 <template>
+  <AppHeader></AppHeader>
 
+  <AppMain></AppMain>
 </template>
 
-<style>
+<style lang="scss">
+  @use './styles/general.scss'
+
 
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default{
+        name:"AppMain",
+        data(){
+            return{
+
+            }
+        },
+        methods:{
+            getWorks(){
+                console.log('ciao');
+
+                axios.get("http://localhost:8000/api/works")
+                .then(response =>{
+                    console.log(response);
+                })
+            }
+        },
+        mounted(){
+            this.getWorks();
+        }
+    }
+
+
+</script>
+
+
+<style>
+
+</style>
