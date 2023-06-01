45e3f91f284b79ca8b4a73c486da98464bc98857
Francesco Rocco <francescorocco97@gmail.com>
2023-06-01 15:19:42 +0200

    esercizio completo sistemato

--- src/pages/SingleWork.vue
+++ src/pages/SingleWork.vue
@@ -1,17 +1,20 @@
 <template>
-<!-- <hr class="border border-danger border-2 opacity-75">
+<hr class="border border-danger border-2 opacity-75">
     <div class="d-flex justify-content-center mt-5">
         <hr class="border border-primary border-3 opacity-75">
         <div class="card text-center text-bg-info" style="width: 18rem;">
             <div class="card-body">
-                <h2 class="card-title pb-2"></h2>
-                <h4 class="card-subtitle mb-2 text-body-secondary"></h4>
-                <p class="card-text"></p>
-                <p class="card-text"></p>
-                <p class="card-text"></p>
+                <h2 class="card-title pb-2">{{ work.title }}</h2>
+                <h4 class="card-subtitle mb-2 text-body-secondary" v-if="work.description">{{ work.description }}</h4>
+                <p class="card-text" v-if="work.languages">{{ work.languages }}</p>
+                <p class="card-text" v-if="work.type">{{ work.type.name }}</p>
+                <p class="card-text" v-if="work.technologies">
+                <span v-for="tec in this.work.technologies">{{ tec.name }}</span>
+            </p>
             </div>
         </div>
-    </div> -->
+        <hr class="border border-primary border-3 opacity-75">
+    </div>
 </template>
 
 <script>
@@ -20,15 +23,17 @@
         name: 'SingleWork',
         data(){
             return{
-                baseUrl: 'http://localhost:8000'
+                baseUrl: 'http://localhost:8000',
+                work: []
             }
         },
         mounted(){
             const id = this.$route.params.id;
 
-            axios.get(`${this.baseUrl}/api/works/${id}`)
+            axios.get(`${this.baseUrl}/api/work/${id}`)
             .then(response => {
                 console.log(response);
+                this.work = response.data.work;
             })
         }
     }
